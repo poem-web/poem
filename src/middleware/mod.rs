c73d2107683ad593b6354610f6b08d11409c23c1
Sunli <scott_s829@163.com>
2021-08-27 15:33:26 +0800

    Add `middleware::make` function to make an middleware with a closure.

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -26,3 +26,74 @@ pub trait Middleware<E: Endpoint> {
     /// Transform the input [`Endpoint`] to another one.
     fn transform(self, ep: E) -> Self::Output;
 }
+
+/// A middleware implemented by a closure.
+pub struct FnMiddleware<T>(T);
+
+impl<T, E, E2> Middleware<E> for FnMiddleware<T>
+where
+    T: Fn(E) -> E2,
+    E: Endpoint,
+    E2: Endpoint,
+{
+    type Output = E2;
+
+    fn transform(self, ep: E) -> Self::Output {
+        (self.0)(ep)
+    }
+}
+
+/// Make middleware with a closure.
+pub fn make<T>(f: T) -> FnMiddleware<T> {
+    FnMiddleware(f)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        handler,
+        http::{header::HeaderName, HeaderValue},
+        EndpointExt, IntoResponse, Request, Response,
+    };
+
+    #[tokio::test]
+    async fn test_make() {
+        #[handler(internal)]
+        fn index() -> &'static str {
+            "abc"
+        }
+
+        struct AddHeader<E> {
+            ep: E,
+            header: HeaderName,
+            value: HeaderValue,
+        }
+
+        #[async_trait::async_trait]
+        impl<E: Endpoint> Endpoint for AddHeader<E> {
+            type Output = Response;
+
+            async fn call(&self, req: Request) -> Self::Output {
+                let mut resp = self.ep.call(req).await.into_response();
+                resp.headers_mut()
+                    .insert(self.header.clone(), self.value.clone());
+                resp
+            }
+        }
+
+        let ep = index.with(make(|ep| AddHeader {
+            ep,
+            header: HeaderName::from_static("hello"),
+            value: HeaderValue::from_static("world"),
+        }));
+        let mut resp = ep.call(Request::default()).await;
+        assert_eq!(
+            resp.headers()
+                .get(HeaderName::from_static("hello"))
+                .cloned(),
+            Some(HeaderValue::from_static("world"))
+        );
+        assert_eq!(resp.take_body().into_string().await.unwrap(), "abc");
+    }
+}
