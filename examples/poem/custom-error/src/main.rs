f6406f5dbecf48b6a4a9d99da9a71075eb786f25
Sunli <scott_s829@163.com>
2021-09-26 10:20:34 +0800

    Add custom-error example

---
+++ examples/poem/custom-error/src/main.rs
@@ -0,0 +1,33 @@
+use poem::{
+    handler, http::StatusCode, listener::TcpListener, route, route::get, web::Json, IntoResponse,
+    Response, Server,
+};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct CustomError {
+    message: String,
+}
+
+impl IntoResponse for CustomError {
+    fn into_response(self) -> Response {
+        Json(self)
+            .with_status(StatusCode::BAD_REQUEST)
+            .into_response()
+    }
+}
+
+#[handler]
+fn hello() -> Result<String, CustomError> {
+    Err(CustomError {
+        message: "custom error".to_string(),
+    })
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = route().at("/", get(hello));
+    let listener = TcpListener::bind("127.0.0.1:3000");
+    let server = Server::new(listener).await?;
+    server.run(app).await
+}
