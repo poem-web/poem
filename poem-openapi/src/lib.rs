a5393401b21f5fb10f979d9ae7710a5fe0ed37e1
Sunli <scott_s829@163.com>
2021-10-14 07:57:38 +0800

    Update lib.rs

--- poem-openapi/src/lib.rs
+++ poem-openapi/src/lib.rs
@@ -58,7 +58,7 @@
 //!     let api_service = OpenApiService::new(Api)
 //!         .title("Hello World")
 //!         .server("http://localhost:3000/api");
-//!     let ui = api_service.swagger_ui("http://localhost:3000");
+//!     let ui = api_service.swagger_ui();
 //!
 //!     poem::Server::new(listener)
 //!         .await?
