9a3782cad73248c4ee3391fd96907f5d2ae12649
steven.chen <steven.chen@ingka.ikea.com>
2021-10-14 13:27:48 +0800

    20211014 [doc] steven.chen Append some comments on the following sample projects: hello-world json lambda-hello-world middleware

--- examples/poem/lambda-hello-world/src/main.rs
+++ examples/poem/lambda-hello-world/src/main.rs
@@ -1,11 +1,20 @@
 use poem::{get, handler, web::Path, Route};
 use poem_lambda::{run, Error};
 
+/// Real handle method for restful api.
+///
+/// Details ref the doc in hello-world
 #[handler]
 fn hello(Path(name): Path<String>) -> String {
     format!("hello: {}", name)
 }
 
+/// Main method in service.
+///
+/// [`poem_lambda`] was designed to run a lambda server/operator in Amazon Lambda service
+/// [`Route`], [`Route::at`] and other stuffs' attributes are the same struct
+/// [`poem_lambda::run`] would take the responsible of holding the service, monitoring port,
+/// taking action on request .etc
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     if std::env::var_os("RUST_LOG").is_none() {
