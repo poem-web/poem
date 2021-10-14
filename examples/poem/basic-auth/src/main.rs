f15c44df9554ea92ace550ff123c53844ca6a35c
Sunli <scott_s829@163.com>
2021-10-14 15:51:04 +0800

    Add `basic-auth` example for poem.

---
+++ examples/poem/basic-auth/src/main.rs
@@ -0,0 +1,68 @@
+use poem::{
+    get, handler,
+    http::StatusCode,
+    listener::TcpListener,
+    web::{
+        headers,
+        headers::{authorization::Basic, HeaderMapExt},
+    },
+    Endpoint, EndpointExt, Error, Middleware, Request, Result, Route, Server,
+};
+
+struct BasicAuth {
+    username: String,
+    password: String,
+}
+
+impl<E: Endpoint> Middleware<E> for BasicAuth {
+    type Output = BasicAuthEndpoint<E>;
+
+    fn transform(&self, ep: E) -> Self::Output {
+        BasicAuthEndpoint {
+            ep,
+            username: self.username.clone(),
+            password: self.password.clone(),
+        }
+    }
+}
+
+struct BasicAuthEndpoint<E> {
+    ep: E,
+    username: String,
+    password: String,
+}
+
+#[poem::async_trait]
+impl<E: Endpoint> Endpoint for BasicAuthEndpoint<E> {
+    type Output = Result<E::Output>;
+
+    async fn call(&self, req: Request) -> Self::Output {
+        if let Some(auth) = req.headers().typed_get::<headers::Authorization<Basic>>() {
+            if auth.0.username() == self.username && auth.0.password() == self.password {
+                return Ok(self.ep.call(req).await);
+            }
+        }
+        Err(Error::new(StatusCode::UNAUTHORIZED))
+    }
+}
+
+#[handler]
+fn index() -> &'static str {
+    "hello"
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "poem=debug");
+    }
+    tracing_subscriber::fmt::init();
+
+    let app = Route::new().at("/", get(index)).with(BasicAuth {
+        username: "test".to_string(),
+        password: "123456".to_string(),
+    });
+    let listener = TcpListener::bind("127.0.0.1:3000");
+    let server = Server::new(listener).await?;
+    server.run(app).await
+}
--- poem-openapi/CHANGELOG.md
+++ poem-openapi/CHANGELOG.md
@@ -4,7 +4,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## Unreleased
+# [1.0.3] 2010-10-14
 
 - Add `prefix_path` and `tag` attributes for `#[OpenApi]`. [#57](https://github.com/poem-web/poem/pull/57)
 - `OpenApiService::swagger_ui` method no longer needs the `absolute_uri` parameter.
