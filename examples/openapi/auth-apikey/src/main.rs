c21535d85eb1219a3bd3ef12066d6290e71d94c1
Sunli <scott_s829@163.com>
2022-01-05 13:49:07 +0800

    Update examples

--- examples/openapi/auth-apikey/src/main.rs
+++ examples/openapi/auth-apikey/src/main.rs
@@ -1,8 +1,22 @@
-use poem::{http::StatusCode, listener::TcpListener, Error, Request, Result, Route};
-use poem_openapi::{
-    auth::ApiKey, param::Query, payload::PlainText, OpenApi, OpenApiService, SecurityScheme,
+use hmac::{Hmac, NewMac};
+use jwt::{SignWithKey, VerifyWithKey};
+use poem::{
+    error::InternalServerError, listener::TcpListener, web::Data, EndpointExt, Request, Result,
+    Route,
 };
+use poem_openapi::{
+    auth::ApiKey,
+    payload::{Json, PlainText},
+    Object, OpenApi, OpenApiService, SecurityScheme,
+};
+use serde::{Deserialize, Serialize};
+use sha2::Sha256;
 
+const SERVER_KEY: &[u8] = b"123456";
+
+type ServerKey = Hmac<Sha256>;
+
+#[derive(Debug, Serialize, Deserialize)]
 struct User {
     username: String,
 }
@@ -17,10 +31,15 @@ struct User {
 )]
 struct MyApiKeyAuthorization(User);
 
-async fn api_checker(_: &Request, api_key: ApiKey) -> Option<User> {
-    api_key.key.strip_prefix("key:").map(|username| User {
-        username: username.to_string(),
-    })
+async fn api_checker(req: &Request, api_key: ApiKey) -> Option<User> {
+    let server_key = req.data::<ServerKey>().unwrap();
+    VerifyWithKey::<User>::verify_with_key(api_key.key.as_str(), server_key).ok()
+}
+
+#[derive(Object)]
+#[oai(inline)]
+struct LoginRequest {
+    username: String,
 }
 
 struct Api;
@@ -29,9 +48,18 @@ struct Api;
 #[allow(unused_variables)]
 impl Api {
     /// This is just a demo, so you can log in with any username and password.
-    #[oai(path = "/login", method = "get")]
-    async fn login(&self, user: Query<String>, password: Query<String>) -> PlainText<String> {
-        PlainText(format!("key:{}", user.0))
+    #[oai(path = "/login", method = "post")]
+    async fn login(
+        &self,
+        server_key: Data<&ServerKey>,
+        req: Json<LoginRequest>,
+    ) -> Result<PlainText<String>> {
+        let token = User {
+            username: req.0.username,
+        }
+        .sign_with_key(server_key.0)
+        .map_err(InternalServerError)?;
+        Ok(PlainText(token))
     }
 
     /// This API returns the currently logged in user.
@@ -51,8 +79,13 @@ async fn main() -> Result<(), std::io::Error> {
     let api_service =
         OpenApiService::new(Api, "Authorization Demo", "1.0").server("http://localhost:3000/api");
     let ui = api_service.swagger_ui();
+    let server_key = Hmac::<Sha256>::new_from_slice(SERVER_KEY).expect("valid server key");
+    let app = Route::new()
+        .nest("/api", api_service)
+        .nest("/", ui)
+        .data(server_key);
 
     poem::Server::new(TcpListener::bind("127.0.0.1:3000"))
-        .run(Route::new().nest("/api", api_service).nest("/", ui))
+        .run(app)
         .await
 }
