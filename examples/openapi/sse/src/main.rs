37813caaea2d3b9334f7937ecb26aa797bc3e3cb
Sunli <scott_s829@163.com>
2022-01-11 15:03:39 +0800

    Add example-openapi-sse #160

---
+++ examples/openapi/sse/src/main.rs
@@ -0,0 +1,43 @@
+use futures_util::{stream::BoxStream, StreamExt};
+use poem::{listener::TcpListener, Route, Server};
+use poem_openapi::{payload::EventStream, Object, OpenApi, OpenApiService};
+use tokio::time::Duration;
+
+#[derive(Object)]
+struct Event {
+    value: i32,
+}
+
+struct Api;
+
+#[OpenApi]
+impl Api {
+    #[oai(path = "/events", method = "get")]
+    async fn index(&self) -> EventStream<BoxStream<'static, Event>> {
+        EventStream::new(
+            async_stream::stream! {
+                for i in 0.. {
+                    tokio::time::sleep(Duration::from_secs(1)).await;
+                    yield Event { value: i };
+                }
+            }
+            .boxed(),
+        )
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "poem=debug");
+    }
+    tracing_subscriber::fmt::init();
+
+    let api_service =
+        OpenApiService::new(Api, "Hello World", "1.0").server("http://localhost:3000/api");
+    let ui = api_service.swagger_ui();
+
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .run(Route::new().nest("/api", api_service).nest("/", ui))
+        .await
+}
