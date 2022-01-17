210730c72e7e14bc49c1b8bd4e03e95cde6ea26f
Sunli <scott_s829@163.com>
2022-01-17 16:38:27 +0800

    Update examples

--- examples/openapi/union/src/main.rs
+++ examples/openapi/union/src/main.rs
@@ -36,7 +36,7 @@ async fn main() -> Result<(), std::io::Error> {
     }
     tracing_subscriber::fmt::init();
 
-    let api_service = OpenApiService::new(Api, "Anyof", "1.0").server("http://localhost:3000/api");
+    let api_service = OpenApiService::new(Api, "Union", "1.0").server("http://localhost:3000/api");
     let ui = api_service.swagger_ui();
     let spec = api_service.spec_endpoint();
 
