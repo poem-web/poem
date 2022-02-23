0b85414ee1904fb7e959604ab3314390d1493240
Sunli <scott_s829@163.com>
2022-02-23 19:45:09 +0800

    b

--- poem/src/cache/cache_storage.rs
+++ poem/src/cache/cache_storage.rs
@@ -2,18 +2,20 @@ use std::time::Duration;
 
 use bytes::Bytes;
 
+use crate::Result;
+
 /// Represents a back-end cache storage.
 #[async_trait::async_trait]
 pub trait CacheStorage: Send + Sync {
-    async fn set(&self, key: &[u8], value: &[u8], timeout: Option<Duration>, version: u64);
+    async fn set(&self, key: Bytes, value: Bytes, expires_in: Option<Duration>) -> Result<()>;
 
-    async fn get(&self, key: &[u8], version: u64) -> Option<Bytes>;
+    async fn get(&self, key: Bytes) -> Result<Option<Bytes>>;
 
-    async fn touch(&self, key: &[u8], version: i64);
+    async fn touch(&self, key: Bytes, expires_in: Option<Duration>) -> Result<()>;
 
-    async fn delete(&self, key: &[u8], version: i64);
+    async fn delete(&self, key: Bytes) -> Result<()>;
 
-    async fn contains_key(&self, key: &[u8], version: i64) -> bool;
+    async fn contains_key(&self, key: Bytes) -> Result<bool>;
 
-    async fn clear(&self, key: &[u8]);
+    async fn clear(&self) -> Result<()>;
 }
--- poem/src/cache/memory_storage.rs
+++ poem/src/cache/memory_storage.rs
@@ -1,37 +1,70 @@
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use bytes::Bytes;
 use lru::LruCache;
+use parking_lot::Mutex;
 
-use crate::cache::CacheStorage;
+use crate::{cache::CacheStorage, Result};
+
+struct Entry {
+    value: Bytes,
+    expired_at: Option<Instant>,
+}
+
+impl Entry {
+    fn is_expired(&self) -> bool {
+        let now = Instant::now();
+        match &self.expired_at {
+            Some(expired_at) => &now > expired_at,
+            None => false,
+        }
+    }
+}
 
 pub struct MemoryStorage {
-    lru: LruCache<Bytes, Bytes>,
+    lru: Mutex<LruCache<Bytes, Entry>>,
 }
 
 #[async_trait::async_trait]
 impl CacheStorage for MemoryStorage {
-    async fn set(&self, key: &[u8], value: &[u8], timeout: Option<Duration>, version: u64) {
-        todo!()
+    async fn set(&self, key: Bytes, value: Bytes, expires_in: Option<Duration>) -> Result<()> {
+        self.lru.lock().put(
+            key.into(),
+            Entry {
+                value: value.into(),
+                expired_at: expires_in.map(|timeout| Instant::now() + timeout),
+            },
+        );
+        Ok(())
     }
 
-    async fn get(&self, key: &[u8], version: u64) -> Option<Bytes> {
-        todo!()
+    async fn get(&self, key: Bytes) -> Result<Option<Bytes>> {
+        Ok(self
+            .lru
+            .lock()
+            .get(&key)
+            .filter(|entry| entry.is_expired())
+            .map(|entry| entry.value.clone()))
     }
 
-    async fn touch(&self, key: &[u8], version: i64) {
-        todo!()
+    async fn touch(&self, key: Bytes, expires_in: Option<Duration>) -> Result<()> {
+        if let Some(entry) = self.lru.lock().get_mut(&key) {
+            entry.expired_at = expires_in.map(|timeout| Instant::now() + timeout);
+        }
+        Ok(())
     }
 
-    async fn delete(&self, key: &[u8], version: i64) {
-        todo!()
+    async fn delete(&self, key: Bytes) -> Result<()> {
+        self.lru.lock().pop(&key);
+        Ok(())
     }
 
-    async fn contains_key(&self, key: &[u8], version: i64) -> bool {
-        todo!()
+    async fn contains_key(&self, key: Bytes) -> Result<bool> {
+        Ok(self.lru.lock().contains(&key))
     }
 
-    async fn clear(&self, key: &[u8]) {
-        todo!()
+    async fn clear(&self) -> Result<()> {
+        self.lru.lock().clear();
+        Ok(())
     }
 }
