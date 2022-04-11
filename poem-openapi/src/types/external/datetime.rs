73463beab5e6643b6920883e3be06c75928eaaab
Sunli <scott_s829@163.com>
2022-04-11 16:01:16 +0800

    [poem-openapi] implement `Type` for `chrono::NaiveDateTime`, `chrono::NaiveDate`, `chrono::NaiveTime` #252

--- poem-openapi/src/types/external/datetime.rs
+++ poem-openapi/src/types/external/datetime.rs
@@ -1,6 +1,6 @@
 use std::borrow::Cow;
 
-use chrono::{DateTime, FixedOffset, Local, Utc};
+use chrono::{DateTime, FixedOffset, Local, NaiveDate, NaiveDateTime, NaiveTime, Utc};
 use poem::web::Field;
 use serde_json::Value;
 
@@ -78,3 +78,70 @@ macro_rules! impl_datetime_types {
 impl_datetime_types!(DateTime<Utc>, "string", "date-time");
 impl_datetime_types!(DateTime<Local>, "string", "date-time");
 impl_datetime_types!(DateTime<FixedOffset>, "string", "date-time");
+
+macro_rules! impl_naive_datetime_types {
+    ($ty:ty, $type_name:literal, $format:literal) => {
+        impl Type for $ty {
+            const IS_REQUIRED: bool = true;
+
+            type RawValueType = Self;
+
+            type RawElementValueType = Self;
+
+            fn name() -> Cow<'static, str> {
+                concat!($type_name, "(", $format, ")").into()
+            }
+
+            fn schema_ref() -> MetaSchemaRef {
+                MetaSchemaRef::Inline(Box::new(MetaSchema::new_with_format($type_name, $format)))
+            }
+
+            fn as_raw_value(&self) -> Option<&Self::RawValueType> {
+                Some(self)
+            }
+
+            fn raw_element_iter<'a>(
+                &'a self,
+            ) -> Box<dyn Iterator<Item = &'a Self::RawElementValueType> + 'a> {
+                Box::new(self.as_raw_value().into_iter())
+            }
+        }
+
+        impl ParseFromJSON for $ty {
+            fn parse_from_json(value: Option<Value>) -> ParseResult<Self> {
+                let value = value.unwrap_or_default();
+                if let Value::String(value) = value {
+                    Ok(value.parse()?)
+                } else {
+                    Err(ParseError::expected_type(value))
+                }
+            }
+        }
+
+        impl ParseFromParameter for $ty {
+            fn parse_from_parameter(value: &str) -> ParseResult<Self> {
+                Ok(value.parse()?)
+            }
+        }
+
+        #[poem::async_trait]
+        impl ParseFromMultipartField for $ty {
+            async fn parse_from_multipart(field: Option<Field>) -> ParseResult<Self> {
+                match field {
+                    Some(field) => Ok(field.text().await?.parse()?),
+                    None => Err(ParseError::expected_input()),
+                }
+            }
+        }
+
+        impl ToJSON for $ty {
+            fn to_json(&self) -> Option<Value> {
+                Some(Value::String(self.to_string()))
+            }
+        }
+    };
+}
+
+impl_naive_datetime_types!(NaiveDateTime, "string", "naive-date-time");
+impl_naive_datetime_types!(NaiveDate, "string", "naive-date");
+impl_naive_datetime_types!(NaiveTime, "string", "naive-time");
