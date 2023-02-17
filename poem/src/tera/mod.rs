dc5b54c462885ccdda469e15cb7003f9b70df4ec
Galitan-dev <galitan.dev@gmail.com>
2023-02-17 23:35:34 +0100

    docs: add a tera templating handler example in tera module doc

--- poem/src/tera/mod.rs
+++ poem/src/tera/mod.rs
@@ -7,6 +7,20 @@
 //!
 //! let templating = TeraTemplating::from_glob("templates/**/*");
 //! ```
+//! 
+//! # Render a template inside an handler with some context vars
+//!
+//! ```no_run
+//! use poem::{web::Path, tera::TeraTemplate};
+//! use tera::Tera;
+//!
+//! #[handler]
+//! fn hello(Path(name): Path<String>, tera: Tera) -> TeraTemplate {
+//!     let mut context = Context::new();
+//!     context.insert("name", &name);
+//!     tera.render("index.html.tera", &context)
+//! }
+//! ```
 
 mod endpoint;
 mod middleware;
