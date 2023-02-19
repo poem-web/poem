049215cf02c5d4b1ab76f290b4708f3142d6d61b
Yiyu Lin <linyiyu1992@gmail.com>
2023-02-19 12:14:36 +0800

    doc: add `rustdoc::broken_intra_doc_links` warn (#514)

--- poem/src/lib.rs
+++ poem/src/lib.rs
@@ -261,6 +261,7 @@
 #![forbid(unsafe_code)]
 #![deny(private_in_public, unreachable_pub)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
+#![warn(rustdoc::broken_intra_doc_links)]
 #![warn(missing_docs)]
 
 pub mod endpoint;
