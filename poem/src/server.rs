e05452b686418f7465a057e9568485872d073532
Sunli <scott_s829@163.com>
2024-07-19 16:03:54 +0800

    add some methods to Server for HTTP2 settings

--- poem/src/server.rs
+++ poem/src/server.rs
@@ -42,6 +42,8 @@ pub struct Server<L, A> {
     listener: Either<L, A>,
     name: Option<String>,
     idle_timeout: Option<Duration>,
+    http2_max_concurrent_streams: Option<u32>,
+    http2_max_pending_accept_reset_streams: Option<u32>,
 }
 
 impl<L: Listener> Server<L, Infallible> {
@@ -51,6 +53,8 @@ impl<L: Listener> Server<L, Infallible> {
             listener: Either::Listener(listener),
             name: None,
             idle_timeout: None,
+            http2_max_concurrent_streams: None,
+            http2_max_pending_accept_reset_streams: Some(20),
         }
     }
 }
@@ -62,6 +66,8 @@ impl<A: Acceptor> Server<Infallible, A> {
             listener: Either::Acceptor(acceptor),
             name: None,
             idle_timeout: None,
+            http2_max_concurrent_streams: None,
+            http2_max_pending_accept_reset_streams: Some(20),
         }
     }
 }
@@ -87,6 +93,30 @@ where
     pub fn idle_timeout(self, timeout: Duration) -> Self {
         Self {
             idle_timeout: Some(timeout),
+            ..self
+        }
+    }
+
+    /// Sets the [`SETTINGS_MAX_CONCURRENT_STREAMS`][spec] option for HTTP2
+    /// connections.
+    ///
+    /// Default is 200. Passing `None` will remove any limit.
+    ///
+    /// [spec]: https://http2.github.io/http2-spec/#SETTINGS_MAX_CONCURRENT_STREAMS
+    pub fn http2_max_concurrent_streams(self, max: impl Into<Option<u32>>) -> Self {
+        Self {
+            http2_max_concurrent_streams: max.into(),
+            ..self
+        }
+    }
+
+    /// Configures the maximum number of pending reset streams allowed before a GOAWAY will be sent.
+    ///
+    /// This will default to the default value set by the [`h2` crate](https://crates.io/crates/h2).
+    /// As of v0.4.0, it is 20.
+    pub fn http2_max_pending_accept_reset_streams(self, max: impl Into<Option<u32>>) -> Self {
+        Self {
+            http2_max_pending_accept_reset_streams: max.into(),
             ..self
         }
     }
@@ -117,6 +147,8 @@ where
             listener,
             name,
             idle_timeout,
+            http2_max_concurrent_streams,
+            http2_max_pending_accept_reset_streams,
         } = self;
         let name = name.as_deref();
         let alive_connections = Arc::new(AtomicUsize::new(0));
@@ -169,7 +201,17 @@ where
                         let server_graceful_shutdown_token_clone = server_graceful_shutdown_token.clone();
 
                         let spawn_fut = AssertUnwindSafe(async move {
-                            let serve_connection = serve_connection(socket, local_addr, remote_addr, scheme, ep, server_graceful_shutdown_token.clone(), idle_timeout);
+                            let serve_connection = serve_connection(
+                                socket,
+                                local_addr,
+                                remote_addr,
+                                scheme,
+                                ep,
+                                server_graceful_shutdown_token.clone(),
+                                idle_timeout,
+                                http2_max_concurrent_streams,
+                                http2_max_pending_accept_reset_streams,
+                            );
 
                             if timeout.is_some() {
                                 tokio::select! {
@@ -331,6 +373,8 @@ async fn serve_connection(
     ep: Arc<dyn DynEndpoint<Output = Response>>,
     server_graceful_shutdown_token: CancellationToken,
     idle_connection_close_timeout: Option<Duration>,
+    http2_max_concurrent_streams: Option<u32>,
+    http2_max_pending_accept_reset_streams: Option<u32>,
 ) {
     let connection_shutdown_token = CancellationToken::new();
 
@@ -368,7 +412,14 @@ async fn serve_connection(
         None => tokio_util::either::Either::Right(socket),
     };
 
-    let builder = auto::Builder::new(hyper_util::rt::TokioExecutor::new());
+    let mut builder = auto::Builder::new(hyper_util::rt::TokioExecutor::new());
+    let mut builder = builder.http2();
+    let builder = builder
+        .max_concurrent_streams(http2_max_concurrent_streams)
+        .max_pending_accept_reset_streams(
+            http2_max_pending_accept_reset_streams.map(|x| x as usize),
+        );
+
     let conn =
         builder.serve_connection_with_upgrades(hyper_util::rt::TokioIo::new(socket), service);
     futures_util::pin_mut!(conn);
