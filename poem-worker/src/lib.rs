218ed214c6b8dc6238efc7e9fa587f356d451e8b
tiannian <dtiannian@gmail.com>
2025-07-20 13:30:03 +0800

    add cloudflare worker support

---
+++ poem-worker/src/lib.rs
@@ -0,0 +1,10 @@
+pub(crate) mod body;
+pub(crate) mod req;
+
+mod cloudflare;
+pub use cloudflare::*;
+
+mod server;
+pub use server::*;
+
+pub use worker::*;
---
+++ poem-worker/src/server.rs
@@ -0,0 +1,59 @@
+use async_trait::async_trait;
+use poem::endpoint::Endpoint;
+use tokio::sync::OnceCell;
+
+#[worker::event(fetch)]
+async fn fetch(
+    request: worker::Request,
+    _env: worker::Env,
+    _ctx: worker::Context,
+) -> Result<worker::Response, worker::Error> {
+    let cf = request.cf().cloned();
+
+    let http_req = worker::HttpRequest::try_from(request)?;
+    let mut poem_req = crate::req::build_poem_req(http_req)?;
+
+    if let Some(cf) = cf {
+        poem_req.set_data(cf);
+    }
+
+    // TODO: handle error
+    let app = SERVER_INSTANCE.get().unwrap();
+
+    let resp = app.get_poem_response(poem_req).await;
+    let worker_resp = crate::req::build_worker_resp(resp)?;
+    let resp = worker::Response::try_from(worker_resp)?;
+
+    Ok(resp)
+}
+
+#[async_trait]
+trait GetResponseInner: Send + Sync + 'static {
+    async fn get_poem_response(&self, req: poem::Request) -> poem::Response;
+}
+
+#[async_trait]
+impl<E: Endpoint + Send + Sync + 'static> GetResponseInner for E {
+    async fn get_poem_response(&self, req: poem::Request) -> poem::Response {
+        self.get_response(req).await
+    }
+}
+
+pub struct Server {}
+
+type BoxedGetResponseInner = Box<dyn GetResponseInner>;
+
+static SERVER_INSTANCE: OnceCell<BoxedGetResponseInner> = OnceCell::const_new();
+
+impl Server {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn run(&self, app: impl Endpoint + 'static) {
+        SERVER_INSTANCE
+            .set(Box::new(app))
+            .map_err(|_| format!("Server instance can only be set once"))
+            .unwrap();
+    }
+}
